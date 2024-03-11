e0157b9e1d07ab6b9483da9a862ba3f9e3076964
Vitor Aguiar <104656492+Hirynnn@users.noreply.github.com>
2024-03-11 16:38:54 -0300

    Add files via upload

---
+++ userdept/src/main/java/com/example/userdept/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.userdept.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.userdept.entities.User;
+
+public interface UserRepository  extends  JpaRepository<User,Long>{
+	
+	
+
+}
